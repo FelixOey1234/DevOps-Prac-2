b83392e29cdeee14d81c89403136c425c4808412
FelixOey <felix.20@ichat.sp.edu.sg>
2022-12-02 00:54:02 +0800

    bruh

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,14 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input():
+    nums = input("Enter some numbers separated by commas (e.g. 5,67,32): ")
+    split = nums.split(",")
+    print([float(x) for x in split])
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+
+main()
